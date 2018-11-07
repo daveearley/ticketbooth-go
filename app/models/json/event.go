29294ff3bd15fb2c7bdd51b9f98025b5545d89d0
Dave Earley <dave@earley.email>
2018-11-06 23:31:26 -0500

    delete more files

--- app/models/json/event.go
+++
@@ -1,8 +0,0 @@
-package jsonmodel
-
-import "github.com/daveearley/ticketbooth/app/models/generated"
-
-type Event struct {
-	*models.Event
-	Attributes *models.AttendeeSlice `json:"attributes"`
-}
--- app/models/modelx/ticket.go
+++
@@ -1,8 +0,0 @@
-package modelx
-
-import "github.com/daveearley/ticketbooth/app/models/generated"
-
-type Ticket struct {
-	*models.Ticket
-	QuantityAvailable int
-}
